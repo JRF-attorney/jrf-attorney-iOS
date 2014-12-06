6f7d6e0a2e277ea8538a001fbcc23666e45b2dfa
Brian Hu <billy170916@gmail.com>
2014-12-06 16:02:12 +0800

    added bridging header

---
+++ JRF-attorney/JRF-attorney-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  JRF-attorney-Bridging-Header.h
+//  JRF-attorney
+//
+//  Created by Brian Hu on 12/6/14.
+//  Copyright (c) 2014 Brian Hu. All rights reserved.
+//
+
+#ifndef JRF_attorney_JRF_attorney_Bridging_Header_h
+#define JRF_attorney_JRF_attorney_Bridging_Header_h
+
+#import <FacebookSDK/FacebookSDK.h>
+#endif
